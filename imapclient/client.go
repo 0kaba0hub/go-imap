28fed3ddf64dfbfaad1785368b26a03b3dc21bbd
Dejan Strbac <dejan@mercata.com>
2025-10-09 12:33:01 +0200

    Server implementation of METADATA - RFC5464

--- imapclient/client.go
+++ imapclient/client.go
@@ -725,7 +725,6 @@ func (c *Client) readResponseTagged(tag, typ string) (startTLS *startTLSCommand,
 		if !c.dec.ExpectAtom(&code) {
 			return nil, fmt.Errorf("in resp-text-code: %w", c.dec.Err())
 		}
-		// TODO: LONGENTRIES and MAXSIZE from METADATA
 		switch code {
 		case "CAPABILITY": // capability-data
 			caps, err := readCapabilities(c.dec)
@@ -733,6 +732,17 @@ func (c *Client) readResponseTagged(tag, typ string) (startTLS *startTLSCommand,
 				return nil, fmt.Errorf("in capability-data: %w", err)
 			}
 			c.setCaps(caps)
+		case "LONGENTRIES", "MAXSIZE": // METADATA response codes with size parameter
+			var size uint32
+			if !c.dec.ExpectSP() || !c.dec.ExpectNumber(&size) {
+				return nil, fmt.Errorf("in resp-code-metadata: %v", c.dec.Err())
+			}
+			if cmd, ok := cmd.(*GetMetadataCommand); ok {
+				if cmd.data.ResponseCodeData == nil {
+					cmd.data.ResponseCodeData = &imap.MetadataResponseCodeData{}
+				}
+				cmd.data.ResponseCodeData.Size = size
+			}
 		case "APPENDUID":
 			var (
 				uidValidity uint32
--- imapserver/capability.go
+++ imapserver/capability.go
@@ -97,6 +97,11 @@ func (c *Conn) availableCaps() []imap.Cap {
 			imap.CapQResync,   // RFC 7162 §4 — see enable.go, expunge.go, select.go
 		})
 
+		// METADATA capability
+		if _, ok := c.session.(SessionMetadata); ok && available.Has(imap.CapMetadata) {
+			caps = append(caps, imap.CapMetadata)
+		}
+
 		if appendLimitSession, ok := c.session.(SessionAppendLimit); ok {
 			limit := appendLimitSession.AppendLimit()
 			caps = append(caps, imap.Cap(fmt.Sprintf("APPENDLIMIT=%d", limit)))
